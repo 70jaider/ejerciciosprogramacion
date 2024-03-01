8f0a64b3483c8ba408ae6717f79685c30ea970f1
70jaider <puamajaider@gmail.com>
2024-03-01 09:43:16 -0500

    Mi primer commit

---
+++ ejercicio.js
@@ -0,0 +1,23 @@
+var nombre = "jaider"
+var apellido=nombre;
+
+console.log(nombre);
+console.log(apellido);
+console.log(edad);
+
+var num1 = "5";
+var num2 = "3";
+
+var suma = num1 + num2;
+
+console.log(suma);
+
+var tengonovia = true;
+console.log(tengonovia);
+
+console.log(typeof(nombre));
+console.log(typeof(edad));
+console.log(typeof(tengonovia));
+
+
+
